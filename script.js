2fbd9d535fc54de91942bb95482d50b9f3da0305
Goddelsh <goddelsh@ynadex.com>
2020-11-21 20:43:59 +0500

    2. Валидация формы JS. [#371981]

---
+++ script.js
@@ -0,0 +1,40 @@
+
+function validate() {
+    let name = $('#name').val();
+    let surname = $('#sname').val();
+    let gender = getSelectedText("gender");
+    let description = $('#description').val();
+    let result = "";
+    if (name == "") {
+        result += "Name: undefined\n";     
+    } else {
+        result += "Name: " + name + "\n";
+    }
+    if (surname == "") {
+        result += "Surname: undefined\n";     
+    } else {
+        result += "Surname: " + surname + "\n";
+    }
+    if (gender == "") {
+        result += "Gender: undefined\n";     
+    } else {
+        result += "Gender: " + gender + "\n";
+    }
+    if (description == "") {
+        result += "Description: undefined\n";     
+    } else {
+        result += "Description: " + description + "\n";
+    }
+    alert(result);
+    return false;
+}
+
+function getSelectedText(elementId) {
+    var elt = document.getElementById(elementId);
+  
+    if (elt.selectedIndex == -1)
+      return null;
+  
+    return elt.options[elt.selectedIndex].text;
+  }
+  
